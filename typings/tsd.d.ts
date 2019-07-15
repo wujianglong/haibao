ebe62589446e92e9836fb5eccf6b2587be73d6fb
wangyonghao <wangyonghao@rongcloud.cn>
2019-07-15 18:25:05 +0800

    feat: add typings

---
+++ typings/tsd.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="angular-file-upload/angular-file-upload.d.ts" />
+/// <reference path="angular-ui-router/angular-ui-router.d.ts" />
+/// <reference path="angularjs/angular.d.ts" />
+/// <reference path="bootstrap/bootstrap.d.ts" />
+/// <reference path="jquery/jquery.d.ts" />
+/// <reference path="ng-file-upload/ng-file-upload.d.ts" />
